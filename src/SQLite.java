ce059dc1dc07c3a1cbcde1d1d6c458b1f774db31
Mariam <elmirmariam16@gmail.com>
2024-12-06 11:41:56 +0100

    eb

--- src/SQLite.java
+++ src/SQLite.java
@@ -1,2 +1,12 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class SQLite {
+    private Connection connection;
+
+    public List<List<Object>> getQuestionsByCategory(int categoryId){
+        return null;
+    }
 }
