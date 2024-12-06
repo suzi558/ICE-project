23abf9833d790b2f8593ef9770eb5c67758a84c2
Mariam <elmirmariam16@gmail.com>
2024-12-06 11:04:12 +0100

    øndring i questions klassen og categories klassen

--- src/Questions.java
+++ src/Questions.java
@@ -2,12 +2,16 @@ public class Questions {
     private int id;
     private String text;
     private String correctAnswer;
+    private String otherChoice1;
+    private String otherChoice2;
     private int points;
 
     public Questions(int id, String text, String correctAnswer, String otherChoice1, String otherChoice2, int points ) {
         this.id = id;
         this.text = text;
         this.correctAnswer = correctAnswer;
+        this.otherChoice1 = otherChoice1;
+        this.otherChoice2 = otherChoice2;
         this.points = points;
     }
 
@@ -21,6 +25,14 @@ public class Questions {
 
     public String getCorrectAnswer() {
         return correctAnswer;
+    }
+
+    public String getOtherChoice1() {
+        return otherChoice1;
+    }
+
+    public String getOtherChoice2() {
+        return otherChoice2;
     }
 
     public int getPoints() {
