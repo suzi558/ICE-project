c6d3ade98855da33ea74949c214af8af1a28227b
DanielBasco <danielsgeniale@gmail.com>
2024-12-11 09:51:48 +0100

    Beklager kodet i main igen, har tilføjet alle metoderne i SQL til at hente det hele fra tabellen untal id self

--- src/SQLite.java
+++ src/SQLite.java
@@ -61,6 +61,101 @@ public void connect (String url){
         return data;
     }
 
+    public ArrayList<String> getCorrectAnswer(){
+        ArrayList<String> data = new ArrayList<>();
+
+        String correct = "SELECT correctAnswer FROM QUESTIONS";
+
+        try {
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(correct);
+
+            while(rs.next()){
+
+                String row = rs.getString("correctAnswer");
+                data.add(row);
+            }
+
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return data;
+    }
+
+    public ArrayList<String> getOtherChoice(){
+        ArrayList<String> data = new ArrayList<>();
+
+        String otherChoice = "SELECT otherChoice FROM QUESTIONS";
+
+        try {
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(otherChoice);
+
+            while(rs.next()){
+
+                String row = rs.getString("otherChoice");
+                data.add(row);
+            }
+
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return data;
+    }
+
+    public ArrayList<String> getOtherChoice2(){
+        ArrayList<String> data = new ArrayList<>();
+
+        String otherChoice2 = "SELECT otherChoice2 FROM QUESTIONS";
+
+        try {
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(otherChoice2);
+
+            while(rs.next()){
+
+                String row = rs.getString("otherChoice2");
+                data.add(row);
+            }
+
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return data;
+    }
+
+    public ArrayList<String> getPoint(){
+        ArrayList<String> data = new ArrayList<>();
+
+        String point = "SELECT Point FROM QUESTIONS";
+
+        try {
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(point);
+
+            while(rs.next()){
+
+                String row = rs.getString("Point");
+                data.add(row);
+            }
+
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return data;
+    }
 
     }
 
