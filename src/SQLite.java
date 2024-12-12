3e5a4d303a778a6e4db954067c965c0858f5bbab
Mariam <elmirmariam16@gmail.com>
2024-12-12 11:53:32 +0100

    kan ikke helt løse fejlen

--- src/SQLite.java
+++ src/SQLite.java
@@ -50,129 +50,27 @@ public class SQLite {
         return questionsList;
     }
 
-    public ArrayList<String> getCategories(){
-            ArrayList<String> data = new ArrayList<>();
-
-            String categories = "SELECT Category FROM Categories";
-
-            try {
-
-                Statement stmt = conn.createStatement();
-
-                ResultSet rs = stmt.executeQuery(categories);
-
-                while(rs.next()){
-
-                    String row = rs.getString("Category");
-                    data.add(row);
-                }
-
-            }
-            catch(SQLException e){
-                System.out.println(e.getMessage());
-            }
-        return data;
-    }
-
-    public ArrayList<String> getCorrectAnswer(){
+    public ArrayList<String> getCategories() {
         ArrayList<String> data = new ArrayList<>();
 
-        String correct = "SELECT correctAnswer FROM QUESTIONS";
+        String categories = "SELECT Category FROM Categories";
 
         try {
 
             Statement stmt = conn.createStatement();
 
-            ResultSet rs = stmt.executeQuery(correct);
+            ResultSet rs = stmt.executeQuery(categories);
 
-            while(rs.next()){
+            while (rs.next()) {
 
-                String row = rs.getString("correctAnswer");
+                String row = rs.getString("Category");
                 data.add(row);
             }
 
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return data;
     }
-
-    public ArrayList<String> getOtherChoice(){
-        ArrayList<String> data = new ArrayList<>();
-
-        String otherChoice = "SELECT otherChoice FROM QUESTIONS";
-
-        try {
-
-            Statement stmt = conn.createStatement();
-
-            ResultSet rs = stmt.executeQuery(otherChoice);
-
-            while(rs.next()){
-
-                String row = rs.getString("otherChoice");
-                data.add(row);
-            }
-
-        }
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return data;
-    }
-
-    public ArrayList<String> getOtherChoice2(){
-        ArrayList<String> data = new ArrayList<>();
-
-        String otherChoice2 = "SELECT otherChoice2 FROM QUESTIONS";
-
-        try {
-
-            Statement stmt = conn.createStatement();
-
-            ResultSet rs = stmt.executeQuery(otherChoice2);
-
-            while(rs.next()){
-
-                String row = rs.getString("otherChoice2");
-                data.add(row);
-            }
-
-        }
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return data;
-    }
-
-    public ArrayList<String> getPoint(){
-        ArrayList<String> data = new ArrayList<>();
-
-        String point = "SELECT Point FROM QUESTIONS";
-
-        try {
-
-            Statement stmt = conn.createStatement();
-
-            ResultSet rs = stmt.executeQuery(point);
-
-            while(rs.next()){
-
-                String row = rs.getString("Point");
-                data.add(row);
-            }
-
-        }
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return data;
-    }
-
-    }
-
-
-
-
+}
 
